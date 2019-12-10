86d488ee2f7fe2c08e4c5a201d893853a817e776
Lawrence Carreon <l.carreon123@gmail.com>
2019-12-10 14:09:51 +1100

    2019-12-10 Update 1

--- js/main.js
+++ js/main.js
@@ -1,7 +1,23 @@
-$(document).ready(function(){
-    const gameLogic = {
-        gameOnGoing: false,
-        playerOneSymbol: "",
-        playerTwoSymbol: "",
-    };
-});
+const gameLogic = {
+    gameComplete: false,
+    playerOneSymbol: ``,
+    playerTwoSymbol: ``,
+    currentPlayerTurn: ``,
+    
+    restart: function (){
+        this.gameComplete = false;
+        this.playerOneSymbol = ``;
+        this.playerTwoSymbol = ``;
+    },
+
+    switchTurn: function() {
+        
+    },
+
+    gameStart: function (firstPlayer, secondPlayer) {
+            this.playerOneSymbol = firstPlayer;
+            this.playerTwoSymbol = secondPlayer;
+            this.currentPlayerTurn = firstPlayer;
+            userInterface.turnMessage(this.currentPlayerTurn);
+    },
+};
--- js/ui.js
+++ js/ui.js
@@ -1,5 +1,45 @@
+const userInterface = {
+    turnMessage: function(turnPlayerSymbol){
+        this.$userMsg.text(`It is ${turnPlayerSymbol} player's turn!`);
+    },
+
+    clearBoard: function(){
+        $(`td`).html("");
+        const restartMsg = this.$userMsg.text(`A player has restarted the game!`);
+    },
+
+    toggleHolders: function(buttonPressed){
+        if ( buttonPressed.hasClass(`cross`) || buttonPressed.hasClass(`circle`)){
+            this.$buttonHolders.eq(0).hide();
+            this.$buttonHolders.eq(1).show();
+        }else if ( buttonPressed.hasClass(`Restart`)){
+            //gameLogic.restart();
+            this.clearBoard();
+            this.$buttonHolders.eq(0).show();
+            this.$buttonHolders.eq(1).hide();
+        };
+    },
+    
+    checkButton: function(buttonPressed){
+        if (buttonPressed.hasClass(`cross`)){
+            const priority = `X`;
+            gameLogic.gameStart(priority, `O`);
+            this.toggleHolders(buttonPressed);
+        }else if (buttonPressed.hasClass(`circle`)){
+            const priority = `O`;
+            gameLogic.gameStart(priority, `X`);
+            this.toggleHolders(buttonPressed);
+        }else if (buttonPressed.hasClass(`Pass`)){
+            gameLogic.switchTurn();
+        }else{
+            this.toggleHolders(buttonPressed);
+        }   
+    },
+};
 $(document).ready(function(){
-    const userInterface = {
-        
-    };
+    $('.userButton').on('click',function(){
+        userInterface.checkButton($(this));
+    });
+    userInterface.$buttonHolders = $(`.buttonHolder`);
+    userInterface.$userMsg = $(`.userMessages p`);
 });
