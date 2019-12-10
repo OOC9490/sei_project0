b149d728e0a28bab068e6a7933abf42a6c83195f
Lawrence Carreon <l.carreon123@gmail.com>
2019-12-10 20:17:46 +1100

    update before implementing win/loss check

--- js/main.js
+++ js/main.js
@@ -1,23 +1,57 @@
 const gameLogic = {
-    gameComplete: false,
+    gameOnGoing: false,
+    turnComplete: true,
     playerOneSymbol: ``,
     playerTwoSymbol: ``,
     currentPlayerTurn: ``,
+    tilesFilled: 0,
     
-    restart: function (){
-        this.gameComplete = false;
+    winOrDraw: function (turnPlayer){
+        if (this.tilesFilled === 9 && this.gameOnGoing === true){
+            this.restartGame();
+            userInterface.clearBoard();
+            return;
+        }
+        if (this.currentPlayerTurn === this.playerOneSymbol){
+            this.currentPlayerTurn = this.playerTwoSymbol;
+        }else{
+            this.currentPlayerTurn = this.playerOneSymbol;
+        };
+        userInterface.turnMessage(this.currentPlayerTurn, this.gameOnGoing, this.turnComplete);
+        this.turnCompleteToggle();
+        userInterface.currentBoardSpot.addClass(`Used`);
+    },
+
+    turnCompleteToggle: function (){
+        if ( this.turnComplete === false ){
+            this.turnComplete = true;
+        }else{
+            this.turnComplete = false;
+        };
+    },
+
+    restartGame: function (){
+        this.gameOnGoing = false;
+        this.turnComplete = true;
         this.playerOneSymbol = ``;
         this.playerTwoSymbol = ``;
     },
 
-    switchTurn: function() {
-        
+    switchTurn: function(){
+        if ( this.turnComplete ){
+            this.tilesFilled++;
+            this.winOrDraw(this.currentPlayerTurn);
+        }else{
+            userInterface.turnMessage(this.currentPlayerTurn, this.gameOnGoing, this.turnComplete);
+        }
     },
 
     gameStart: function (firstPlayer, secondPlayer) {
-            this.playerOneSymbol = firstPlayer;
-            this.playerTwoSymbol = secondPlayer;
-            this.currentPlayerTurn = firstPlayer;
-            userInterface.turnMessage(this.currentPlayerTurn);
+        this.playerOneSymbol = firstPlayer;
+        this.playerTwoSymbol = secondPlayer;
+        this.currentPlayerTurn = firstPlayer;
+        userInterface.turnMessage(this.currentPlayerTurn, this.gameOnGoing, this.turnComplete);
+        this.gameOnGoing = true;
+        this.turnComplete = false;
     },
 };
--- js/ui.js
+++ js/ui.js
@@ -1,11 +1,25 @@
 const userInterface = {
-    turnMessage: function(turnPlayerSymbol){
-        this.$userMsg.text(`It is ${turnPlayerSymbol} player's turn!`);
+    drawOnce: false,
+    currentBoardSpot: undefined,
+    turnMessage: function(turnPlayerSymbol, gameState, turnState){
+        if ( gameState === true && turnState === false){
+            this.$userMsg.text(`The ${turnPlayerSymbol} player still hasn't taken their turn!`);
+        } else {
+            this.$userMsg.text(`It is ${turnPlayerSymbol} player's turn!`);
+        }
     },
 
     clearBoard: function(){
         $(`td`).html("");
-        const restartMsg = this.$userMsg.text(`A player has restarted the game!`);
+        $(`td`).removeClass("crossFilled circleFilled");
+        this.$userMsg.text(`The game has been restarted!`);
+        this.$userMsg.animate({
+            opacity: 0
+        },1500, function(){
+            userInterface.$userMsg.text(``);
+            userInterface.$userMsg.css(`opacity`, `1`);
+        });
+        this.drawOnce = false;
     },
 
     toggleHolders: function(buttonPressed){
@@ -13,7 +27,7 @@ const userInterface = {
             this.$buttonHolders.eq(0).hide();
             this.$buttonHolders.eq(1).show();
         }else if ( buttonPressed.hasClass(`Restart`)){
-            //gameLogic.restart();
+            gameLogic.restartGame();
             this.clearBoard();
             this.$buttonHolders.eq(0).show();
             this.$buttonHolders.eq(1).hide();
@@ -31,15 +45,49 @@ const userInterface = {
             this.toggleHolders(buttonPressed);
         }else if (buttonPressed.hasClass(`Pass`)){
             gameLogic.switchTurn();
+            this.drawOnce = false;
         }else{
             this.toggleHolders(buttonPressed);
         }   
     },
+
+    fillGrid: function(gridContents, gridLocation){
+        if (gridContents === ``){
+            if (gameLogic.currentPlayerTurn === `X`){
+                gridLocation.addClass(`crossFilled`);
+                gridLocation.html(`X`);
+                this.drawOnce = true;
+            }else{
+                gridLocation.addClass(`circleFilled`);
+                gridLocation.html(`O`);
+                this.drawOnce = true;
+            }
+        }else if (gridContents === gameLogic.currentPlayerTurn &&gridLocation.hasClass(`Used`) === false){
+            gridLocation.removeClass(`crossFilled circleFilled`);
+            gridLocation.html(``);
+            this.drawOnce = false;
+            gameLogic.turnCompleteToggle();
+        };
+    },
+
+    drawOrErase: function(gridLocation){
+        if ( gridLocation.html() === `` && this.drawOnce === false && gameLogic.gameOnGoing === true){
+            this.fillGrid(gridLocation.html(), gridLocation);
+            this.currentBoardSpot = gridLocation;
+            gameLogic.turnCompleteToggle();
+        }else if (gameLogic.gameOnGoing === true && gridLocation.html() !== ``) {
+            this.fillGrid(gridLocation.html(), gridLocation);
+            this.currentBoardSpot = undefined;
+        };
+    },
 };
 $(document).ready(function(){
-    $('.userButton').on('click',function(){
+    $(`.userButton`).on(`click`,function(){
         userInterface.checkButton($(this));
     });
+    $(`td`).on(`click`, function(){
+        userInterface.drawOrErase($(this));
+    })
     userInterface.$buttonHolders = $(`.buttonHolder`);
     userInterface.$userMsg = $(`.userMessages p`);
 });
